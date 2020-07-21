5488f88f74f184d82edce64d1260ceb37cb8ba0d
root <aditya.verma1044@gmail.com>
2020-07-21 21:09:28 +0530

    case-insensitive

--- bot.js
+++ bot.js
@@ -5,43 +5,44 @@ client.login("NzM0NDkwMTA2NTYzNjU3ODA4.XxSdPg.OQrPZ6WJutGwc8AJWwlmCJ2SciU");
 
 let ID='733628966665191546';                                   //Guild id axios:733628966665191546 ; guild id cirius:734492621950419025
 
+console.log("Starting");
 let prefix="X!";
 console.log(ID);
 client.on("message", (message) => {
-if(message.content.startsWith(prefix)){
+if(message.content.toUpperCase().startsWith(prefix)){                                                //check for prefix
        console.log(message.content);
-       const args = message.content.slice(prefix.length).trim().split(' ');
+       const args = message.content.slice(prefix.length).trim().split(' ');                          //remove prefix and make array of rest words in message
        let input = args[0];
-       if(input=="send"){
+       if(input.toLowerCase()=="send"){
            len=args.length;
-           const chnl = client.guilds.cache.get(ID).channels.cache.find(channel => channel.name === args[1]+"-2020")
+           const chnl = client.guilds.cache.get(ID).channels.cache.find(channel => channel.name === args[1].toLowerCase()+"-2020")
            post=args.slice(2,len+1).join(" ");
-           chnl.send(post+" <@"+message.member.user.id+">");
+           chnl.send(post+"[<@"+message.member.user.id+"> from <#"+message.channel.id+">]");
            message.channel.send("Sent!");
 	   }
-       if(input=="list"){
+       if(input.toLowerCase()=="list"){
            const listchannel = []
            client.guilds.cache.get(ID).channels.cache.forEach(channel => listchannel.push(channel.name));
            len=listchannel.length;
            message.channel.send("These are the channels on the server:\n*"+listchannel.slice(3,len+1).join('\n*'));
            }
-       if(input=="help"){
-           message.channel.send("Every command must be started with **X!**.\nThese commands can be used:")
-           message.channel.send("```bash\n'X! send channel_name message'\n```   =>This command can be used to send message to the channel which you are not currently part of.**For Ex:X! send web-dev your_text**.\nChannels in which you can send message with this: competitive-programming, android-development ,cyber-security ,design ,machine-learning ,web-dev")
-           message.channel.send("```bash\n'X! help'\n```  =>To open this help dialog")
+       if(input.toLowerCase()=="help"){
+           message.channel.send("Every command must be started with :point_right: **X!** :point_left:.\nThese commands can be used:")
+           message.channel.send("```bash\n'X! send channel_name message'\n```   =>This command can be used to send message to the channel which you are not currently part of.**For Ex:X! send web-dev your_text**.\nChannels in which you can send message with this: competitive-programming, android-development ,cyber-security ,design ,machine-learning ,web-dev ,team-eduthon")
+           message.channel.send("```bash\n'X! help'\n```   =>To open this help dialog")
            }    
 }
 
-if(message.guild == null && message.content.startsWith(prefix)) {                                              //get the private message and checck for prefix
+if(message.guild == null && message.content.toUpperCase().startsWith(prefix)) {                                              //Check if it is private message and check for prefix
 const args = message.content.slice(prefix.length).trim().split(' ');                                           //extract words from the sentence provided
 console.log(message.content);
 let input = args[0];
-if(input == "cname"){  										             //check whether to change the name 
+if(input.toLowerCase() == "cname"){  										             //check whether to change the name 
     change=args[1];
     client.guilds.cache.get(ID).members.cache.get(message.author.id).setNickname(change);
     message.author.send("Which Batch you belong to:(Ex X! year 2019)");
     }
-if ( input == "year"){                                                                                         //check for year             
+if ( input.toLowerCase() == "year"){                                                                                         //check for year             
     if( args[1] == "2019" ){
            client.guilds.cache.get(ID).members.cache.get(message.author.id).roles.add('734681077549105233');
            message.author.send("Role Assigned"); 	
