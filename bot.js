1de954d084cf7873a76603d58d0712c82e1443f4
root <aditya.verma1044@gmail.com>
2020-08-08 21:47:53 +0530

    leaderboard

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ console.log("Starting");
 let prefix="X!";
 console.log(ID);
 client.on("ready", () =>{
-    const z = schedule.scheduleJob({hour: 21, minute: 46}, () => {
+    const z = schedule.scheduleJob({hour: 21, minute: 49}, () => {
           console.log("Inside");
           database(client,"show");
           }); 
